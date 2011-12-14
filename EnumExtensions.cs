867e7eff8846eb89a45fd948defc3cfc57f44818
Chris Koenig <chris.koenig@microsoft.com>
2011-12-14 10:15:35 -0600

    initial

---
+++ EnumExtensions.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace PhoneApp85
+{
+    public static class EnumExtensions
+    {
+        public static T[] GetValues<T>(this T value)
+        {
+            Type type = value.GetType();
+
+            if (!type.IsEnum)
+            {
+                string stringFormat = String.Format("Type '{0}' is not an enum", type.Name);
+                throw new ArgumentException(stringFormat);
+            }
+
+            List<T> values = new List<T>();
+
+            var fields = from field in type.GetFields()
+                         where field.IsLiteral
+                         select field;
+
+            foreach (FieldInfo field in fields)
+            {
+                object val = field.GetValue(type);
+                values.Add((T)val);
+            }
+
+            return values.ToArray();
+        }
+    }
+}
---
+++ ViewModel.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Reflection;
+using System.Windows.Input;
+
+namespace PhoneApp85
+{
+    public class ViewModel : INotifyPropertyChanged
+    {
+        // Fields...
+        private InputScopeNameValue _SelectedInputScopeNameValue;
+        private ObservableCollection<InputScopeNameValue> _InputScopeList;
+
+        public ObservableCollection<InputScopeNameValue> InputScopeList
+        {
+            get { return _InputScopeList; }
+            set
+            {
+                if (_InputScopeList == value)
+                    return;
+                _InputScopeList = value;
+                RaisePropertyChanged("InputScopeList");
+            }
+        }
+
+        public InputScopeNameValue SelectedInputScopeNameValue
+        {
+            get { return _SelectedInputScopeNameValue; }
+            set
+            {
+                _SelectedInputScopeNameValue = value;
+                RaisePropertyChanged("SelectedInputScopeNameValue");
+                RaisePropertyChanged("SelectedInputScope");
+            }
+        }
+
+        public InputScope SelectedInputScope
+        {
+            get
+            {
+                InputScope keyboard = new InputScope();
+                InputScopeName name = new InputScopeName();
+
+                try
+                {
+                    name.NameValue = SelectedInputScopeNameValue;
+                    keyboard.Names.Add(name);
+                    return keyboard;
+                }
+                catch (Exception ex)
+                {
+                    name.NameValue = InputScopeNameValue.Default;
+                    keyboard.Names.Add(name);
+                    return keyboard;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the ViewModel class.
+        /// </summary>
+        public ViewModel()
+        {
+            var data = InputScopeNameValue.AddressCity.GetValues<InputScopeNameValue>().ToList();
+            InputScopeList = new ObservableCollection<InputScopeNameValue>(data);
+        }
+
+        #region INPC
+
+        public void RaisePropertyChanged(string name)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(name));
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        #endregion INPC
+    }
+}
